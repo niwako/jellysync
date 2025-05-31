e7cdf0238066c1b84cd5d5544c9c6cde7f127106
Shad Sharma <shadanan@gmail.com>
2025-05-31 16:47:49 -0700

    Make version flags instead of subcommands

--- src/jellysync/__init__.py
+++ src/jellysync/__init__.py
@@ -23,41 +23,43 @@ signal.signal(signal.SIGINT, signal_handler)
 async def run():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--config", help="The config name to use from ~/.jellysync")
-    parser.add_argument("--host", help="The Jellyfin host URL")
-    parser.add_argument("--token", help="The Jellyfin API key")
-    parser.add_argument("--user-id", help="The User ID")
-    parser.add_argument("--media-dir", help="The output folder, e.g. /mnt/media")
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"%(prog)s {importlib.metadata.version('jellysync')}",
+        help="show jellysync version number and exit",
+    )
+
+    parser.add_argument("--config", help="config name to use from ~/.jellysync")
+    parser.add_argument("--host", help="Jellyfin host URL")
+    parser.add_argument("--token", help="Jellyfin API key")
+    parser.add_argument("--user-id", help="User ID")
+    parser.add_argument("--media-dir", help="output folder, e.g. /mnt/media")
 
     parser.add_argument(
         "--dry-run",
-        help="Do a dry run without downloading",
+        help="dry run without downloading",
         action="store_true",
     )
     parser.add_argument(
         "--debug",
-        help="Print HTTP requests and responses to and from Jellyfin server",
+        help="print HTTP requests and responses to and from Jellyfin server",
         action="store_true",
     )
 
-    subparsers = parser.add_subparsers(title="subcommands", required=True)
-
-    version_parser = subparsers.add_parser("version")
-    version_parser.set_defaults(cmd="version")
+    subparsers = parser.add_subparsers(title="commands", dest="cmd")
 
     login_parser = subparsers.add_parser("login")
-    login_parser.set_defaults(cmd="login")
-    login_parser.add_argument("--name", help="The name of the configuration")
-    login_parser.add_argument("--host", help="The Jellyfin host URL")
-    login_parser.add_argument("--user", help="The user name, e.g. jellyfin")
-    login_parser.add_argument("--media-dir", help="The output folder, e.g. /mnt/media")
+    login_parser.add_argument("--name", help="name of the configuration")
+    login_parser.add_argument("--host", help="Jellyfin host URL")
+    login_parser.add_argument("--user", help="user name, e.g. jellyfin")
+    login_parser.add_argument("--media-dir", help="output folder, e.g. /mnt/media")
 
     info_parser = subparsers.add_parser("info")
-    info_parser.set_defaults(cmd="info")
-    info_parser.add_argument("item_id", help="The Jellyfin item ID")
+    info_parser.add_argument("item_id", help="Jellyfin item ID")
 
     list_parser = subparsers.add_parser("list")
-    list_parser.set_defaults(cmd="list")
     list_parser.add_argument(
         "--movie", dest="types", action="append_const", const="Movie"
     )
@@ -69,8 +71,7 @@ async def run():
     )
 
     search_parser = subparsers.add_parser("search")
-    search_parser.set_defaults(cmd="search")
-    search_parser.add_argument("query", help="The search query")
+    search_parser.add_argument("query", help="search query")
     search_parser.add_argument(
         "--movie", dest="types", action="append_const", const="Movie"
     )
@@ -82,15 +83,14 @@ async def run():
     )
 
     download_parser = subparsers.add_parser("download")
-    download_parser.set_defaults(cmd="download")
-    download_parser.add_argument("item_id", help="The Jellyfin item ID")
+    download_parser.add_argument("item_id", help="Jellyfin item ID")
 
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
-    if args.cmd == "version":
-        print(importlib.metadata.version("jellysync"))
-        return
+    if args.cmd is None:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
     configs = JellySyncConfigs.load()
 
